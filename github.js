ab3aa1ad41bf2d83fcc92fe29ab19d404ff31f3a
Thomas Stewart <tjs8rj@virginia.edu>
2019-03-24 17:11:25 -0400

    Actual Code

---
+++ github.js
@@ -0,0 +1,33 @@
+/*
+Exercise File for Week 1 Source, see Week1.txt for details
+*/
+
+/*
+These are functions, but why the weird syntax?
+*/
+const personOne = favoriteFoods => {
+  /*insert functionality here*/
+};
+
+const personTwo = favoriteMovies => {
+  for(let i=0;i<3;i++){
+    console.log(favoriteMovies[i])
+  }
+};
+
+const personThree = favoriteAnimals => {
+  /*insert functionality here*/
+};
+
+/*
+What does this variable syntax do?
+*/
+const favoriteFoods = [],
+  favoriteMovies = ["Avengers 1", "Avengers 2", "Avengers 3", "Avengers 4"],
+  favoriteAnimals = [];
+
+const iceBreaker = () => {
+  /*
+    Call each function here
+    */
+};
